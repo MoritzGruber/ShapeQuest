3484cef459e1788b82c42875399d8a624aa2b5f7
Sven Kirsch <kirsch_sven@web.de>
2016-06-02 15:12:17 +0200

    Added Player

---
+++ Assets/Scripts/Gameplay/playerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class playerController : NetworkBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (!isLocalPlayer)
+            return;
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+    }
+}
