dd183b50e16e7fcf171c77f7f1f1d61bf6f2ac03
L0stW0lf <lostwolf@gmx.net>
2016-06-02 15:35:05 +0200

    exposed InputField text  as strings in UIManager Script

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -1,15 +1,32 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class UIManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public InputField playerNameInput;
+    public InputField playerColorInput;
+    public InputField serverIPInput;
+    public InputField serverPortInput;
+
+
+    public string playerName;
+    public string playerColor;
+    public string serverIP;
+    public string serverPort;
+
+    // Use this for initialization
+    void Start () {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
-	}
+	void Update ()
+    {
+        playerName = playerNameInput.text;
+        playerColor = playerColorInput.text;
+        serverIP    = serverIPInput.text;
+        serverPort  = serverPortInput.text;
+
+    }
 }
